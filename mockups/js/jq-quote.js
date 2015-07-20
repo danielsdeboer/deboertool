afc763aa314fbcec22a5994dbaa3a8b41d5dd3b1
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2015-07-20 16:24:23 -0400

    Refactored some CSS/SCSS, deleted some unused images

--- mockups/js/jq-quote.js
+++ mockups/js/jq-quote.js
@@ -9,11 +9,23 @@ $(document).ready(function() {
     $(this).animate({'width': '20em', 'height': 'auto', 'min-height': '1em'}, 250);
   });
 
-  $('#throughfeed-button').click(function() {
+  $(document).on('click', '#tf', function() {
     $.get('_component_inline-process.html', function(html_payload) {
-      $(html_payload).hide().appendTo('#throughfeed-load-target').fadeIn();
+      $(html_payload).hide().appendTo('#tf-target').fadeIn();
+
+      $('.grow').focus(function() {
+        $(this).animate({'width': '30em'}, 250);
+      });
+
+      $('.grow').blur(function() {
+        $(this).animate({'width': '20em', 'height': 'auto', 'min-height': '1em'}, 250);
+      });
     });
   });
+
 });
+
+
+
 
 
